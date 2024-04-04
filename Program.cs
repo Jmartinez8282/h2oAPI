8ec92bbff5d426ea83d6f83563da41c75c8630eb
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2024-04-04 12:31:19 -0700

    Added JWT authentication

--- Program.cs
+++ Program.cs
@@ -1,11 +1,29 @@
 using System;
 using Microsoft.Extensions.Options;
-
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 //Configuring my connection string by adding DbContext  and use SQLServer
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer("MyConnectionString"));
+
+//JWT authentication
+builder.Services.AddAuthentication("JWT").AddJwtBearer("JWT",Options =>
+{
+    Options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateIssuerSigningKey = false,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySecurityKey")),
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+
+    };
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- obj/Debug/net9.0/h2oAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/h2oAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("h2oAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f65102cbaacd835c811ef18e7b0190a26663cd3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebcac2ecec21d5091e971eb8efff41e03ad5a1d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("h2oAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("h2oAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
