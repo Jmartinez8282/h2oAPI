ebcac2ecec21d5091e971eb8efff41e03ad5a1d2
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2024-04-04 10:52:10 -0700

    Added EF core

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using System;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Configuring my connection string by adding DbContext  and use SQLServer
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- obj/Debug/net9.0/h2oAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/h2oAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("h2oAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+381490e6ccdbc67bcefa4f824ea23eace5c85ecc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f65102cbaacd835c811ef18e7b0190a26663cd3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("h2oAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("h2oAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
