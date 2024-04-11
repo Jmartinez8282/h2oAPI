af651c40f5951fcb82d2ccb1f1f0a0c5c8e75c1c
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2024-04-10 23:05:17 -0700

    updated tables adding our Salt and Hash

--- obj/Debug/net9.0/h2oAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/h2oAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("h2oAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf0d83590756687c8369e7b90e9a11c5a582987")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f28284649e9cd559821c099893518826efe48bd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("h2oAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("h2oAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
